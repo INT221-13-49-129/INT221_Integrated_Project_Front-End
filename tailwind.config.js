0782b216c29e24a44a95fc79ecf45789a953260f
chuphom <priyanut.buadum.11.2.2544@gmail.com>
2021-04-26 01:02:56 +0700

    3.Navbar : App.vue

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,15 @@ module.exports = {
   purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      colors:{
-       orange: '#ff9f58',
-       yell: '#f6d895',
-       koko: '#362c29',
-       brown: '#996550'
+  extend: {
+    colors:{
+     orange: '#ff9f58',
+     yell: '#f6d895',
+     koko: '#362c29',
+     brown: '#996550'
 
-      }
-    },
+    }
+  },
   },
   variants: {
     extend: {},
