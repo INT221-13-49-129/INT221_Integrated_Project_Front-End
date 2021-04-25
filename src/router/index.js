4a7998ed06cb1c8c79c27dfa276ef9a097ede8ca
chuphom <priyanut.buadum.11.2.2544@gmail.com>
2021-04-25 23:28:51 +0700

    2.nav bar link

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Store from '../views/Store.vue'
+
+
 
 const routes = [
   {
@@ -14,6 +17,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/store',
+    name: 'Store',
+    component: Store
   }
 ]
 
---
+++ src/views/Store.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="store">
+    <h1>This is an about pageeee</h1>
+  </div>
+</template>
