d83de49cc388ed201d547d65efbe2805ac566338
Kamin124712T <kamin.124712@mail.kmutt.ac.th>
2021-04-28 00:15:30 +0700

    Upadete App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,57 @@
 <template>
-  <div id="nav" class="bg-red-600 h-1 w-full bg-gradient-to-r to-blue-900 via-red-500 from-purple-800"></div>
-  <div id="nav" class="bg-gray-900 w-auto h-1/2 mx-auto flex flex-row item-center font-mono shadow-2xl">
-    <ul class="text-white">
-      <li class=" px-10 py-1.5 font-normal text-3xl"><router-link to="/"><span><b>CARS</b></span></router-link></li></ul>
-    <div class="flex-grow"></div>
-    <ul class="my-auto mr-5 flex flex-row item-end justify-center h-full text-white text-sm">
-      <li class="px-4">
-        <router-link to="/"> <span><b>HOME</b></span></router-link></li>
-      <li class="px-4">
-        <router-link to="/store"><span><b>STORE</b></span></router-link></li>
-      <li class="px-4">
-        <router-link to="/about"><span><b>ABOUT US</b></span></router-link></li>
-    </ul>
+  <div id class="bg-gradient-to-br from-dark to-red-600 h-full xl:w-full w-336 ">
+    <div id="nav" class="bg-gray-900 w-full h-12 font-mono shadow-2xl">
+      <div class="mx-auto flex flex-row item-center w-320">
+        <div class="w-3/12">
+          <router-link to="/">
+            <img src="./assets/logoCar.png" class="mt-1" />
+          </router-link>
+        </div>
+        <ul class="text-white flex-grow text-center">
+          <li class="px-10 py-1.5 font-normal text-3xl">
+            <router-link to="/">
+              <span>
+                <b>CARS</b>
+              </span>
+            </router-link>
+          </li>
+        </ul>
+        <ul class="my-auto flex flex-row justify-end h-full text-white text-base w-3/12">
+          <li class="px-4">
+            <router-link to="/">
+              <span>
+                <b>HOME</b>
+              </span>
+            </router-link>
+          </li>
+          <li class="px-4">
+            <router-link to="/store">
+              <span>
+                <b>STORE</b>
+              </span>
+            </router-link>
+          </li>
+          <li class="pl-4">
+            <router-link to="/about">
+              <span>
+                <b>ABOUT US</b>
+              </span>
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="w-320 mx-auto bgimge">
+      <router-view />
+    </div>
+    <footer class="pt-5 mt-10 bg-black bg-opacity-90 shadow-2xl">
+      
+    </footer>
   </div>
-  <router-view />
 </template>
+<style>
+@import url("https://fonts.googleapis.com/icon?family=Material+Icons");
+.bgimge {
+  background-image: url("./assets/Tire.png");
+}
+</style>
