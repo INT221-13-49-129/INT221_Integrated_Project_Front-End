4cb586bebba48e5a1c7d62c03824ce8876d27d15
chuphom <priyanut.buadum.11.2.2544@gmail.com>
2021-04-28 02:38:52 +0700

    edit box input Add.Vue

--- src/views/Add.vue
+++ src/views/Add.vue
@@ -16,21 +16,21 @@
                 </div>
                 <div class="container flex flex-col mt-5 text-gray-700 font-semibold ">
                     <div class="text-lg  mt-2 flex flex-row"> <label class="w-max"> NAME : </label> <input
-                            class="flex-grow mr-4 ml-2 rounded-lg shadow-xl w-3/4"> </div>
+                            class="flex-grow mr-4 ml-2 rounded-lg shadow-xl w-3/4 bg-white bg-opacity-50"> </div>
                     <div class="text-lg  mt-2 flex flex-row"> <label class="w-max">BRAND : </label><input
-                            class="flex-grow mr-4 ml-2 rounded-lg shadow-xl w-3/4"> </div>
+                            class="flex-grow mr-4 ml-2 rounded-lg shadow-xl w-3/4 bg-white bg-opacity-50"> </div>
                     <div class="text-lg mt-2 flex flex-row"> <label>POWER : </label><input type="number"
-                            class="flex-grow mr-4 ml-2 rounded-lg shadow-xl w-3/4"> </div>
+                            class="flex-grow mr-4 ml-2 rounded-lg shadow-xl w-3/4 bg-white bg-opacity-50"> </div>
                     <div class="text-lg  mt-2 flex flex-row"> <label>TORQUE : </label><input type="number"
-                            class="flex-grow mr-4 ml-2 rounded-lg shadow-xl w-3/4"> </div>
+                            class="flex-grow mr-4 ml-2 rounded-lg shadow-xl w-3/4 bg-white bg-opacity-50"> </div>
                     <div class="text-lg mt-2 flex flex-row"> <label>WEIGHT : </label><input type="number"
-                            class="flex-grow mr-4 ml-2 rounded-lg shadow-xl w-3/4"> </div>
+                            class="flex-grow mr-4 ml-2 rounded-lg shadow-xl w-3/4 bg-white bg-opacity-50"> </div>
                     <div class="text-lg  mt-2 flex flex-row"> <label>DRIVERTRAIN : </label><input type="number"
-                            class="flex-grow mr-4 ml-2 rounded-lg shadow-xl w-3/4"> </div>
+                            class="flex-grow mr-4 ml-2 rounded-lg shadow-xl w-3/4 bg-white bg-opacity-50"> </div>
                     <div class="text-lg mt-2 flex flex-row"> <label>YEAR OF MANUFACTURE : </label><input type="date"
-                            class="flex-grow mr-4 ml-2 rounded-lg shadow-xl w-3/4"> </div>
+                            class="flex-grow mr-4 ml-2 rounded-lg shadow-xl w-3/4 bg-white bg-opacity-50"> </div>
                     <div class="text-lg mt-2 flex flex-row"> <label>DESCRIPTION : </label><textarea
-                            class="flex-grow mr-4 ml-2 rounded-lg shadow-xl h-20 w-3/4"></textarea> </div>
+                            class="flex-grow mr-4 ml-2 rounded-lg shadow-xl h-20 w-3/4 bg-white bg-opacity-50"></textarea> </div>
                     <div class="flex flex-row justify-center mt-5  pt-16 pb-28 ">
                         <button @click="submit" class="font-bold text-xl tracking-wider uppercase px-7 mx-6 h-10 text-white rounded-lg shadow-xl 
              transition duration-500 ease-in-out transform bg-gradient-to-r from-dark to-red-600 hover:from-red-600 hover:to-dark
