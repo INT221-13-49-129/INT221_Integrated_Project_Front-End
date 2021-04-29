33f740ec27d286c4f4e2dd000691367045177952
Patiphan <patiphan.new@mail.kmutt.ac.th>
2021-04-29 17:37:08 +0700

    Add Team

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,5 +1,47 @@
 <template>
-  <div class="team">
-    <h1>This is an about page</h1>
+  <div class="text-white text-center text-5xl mt-10 underline">
+    <h1>RESPONSIBILITY</h1>
+  </div>
+  <div
+    class="flex flex-row mt-10 py-20 px-24 bg-white bg-opacity-25 rounded-2xl mx-auto shadow-2xl"
+  >
+    <div class="w-1/3 container flex flex-col">
+      <div class="text-2xl text-white text-right py-2 px-20">นาย คามิน โต๊ะประดู่</div>
+      <div class="text-2xl text-white text-right py-2">ฝ่ายงานที่รับผิดชอบ</div>
+      <div class="text-2xl text-white text-right py-2 px-10">FRONT-END / BACK-END</div>
+      <div class="text-2xl text-white text-right py-2">นอนกลางวัน/ตื่นกลางคืน</div>
+    </div>
+    <div class="flex-grow"></div>
+    <div class="flex justify-center item-center w-1/3">
+      <img src="../assets/Kamin.png" />
+    </div>
+  </div>
+  <div
+    class="flex flex-row mt-10 py-20 px-24 bg-white bg-opacity-25 rounded-2xl mx-auto shadow-2xl"
+  >
+    <div class="flex justify-center item-center w-1/3">
+      <img src="../assets/New.png" />
+    </div>
+    <div class="flex-grow"></div>
+    <div class="w-1/3 container flex flex-col">
+      <div class="text-2xl text-white mx-12 py-2">นาย ปฏิภาณ เทียนสุวรรณ</div>
+      <div class="text-2xl text-white py-2">ฝ่ายงานที่รับผิดชอบ</div>
+      <div class="text-2xl text-white mx-10">NETWORK / DEVOP</div>
+      <div class="text-2xl text-white py-2">นอนกลางคืน/ตื่นกลางวัน</div>
+    </div>
+  </div>
+  <div
+    class="flex flex-row mt-10 py-20 px-24 bg-white bg-opacity-25 rounded-2xl mx-auto shadow-2xl"
+  >
+    <div class="w-1/3 container flex flex-col">
+      <div class="text-2xl text-white text-right mx-16 py-2">นางสาว ปรียานุช บัวดำ</div>
+      <div class="text-2xl text-white text-right py-2">ฝ่ายงานที่รับผิดชอบ</div>
+      <div class="text-2xl text-white text-right mx-10 py-2">FRONT-END / DATABASE</div>
+      <div class="text-2xl text-white text-right py-2">นอนทั้งกลางคืนทั้งกลางวัน</div>
+    </div>
+    <div class="flex-grow"></div>
+    <div class="flex justify-center item-center w-1/3">
+      <img src="../assets/cchom.jpg" />
+    </div>
   </div>
 </template>
