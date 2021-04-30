a7f9c85aac356a6e7fff9142d3825edb00f8054e
chuphom <priyanut.buadum.11.2.2544@gmail.com>
2021-04-30 23:35:38 +0700

    edit 404 page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,3 +1,16 @@
 <template>
-    <div>404</div>
+    <div class="bg-white rounded-3xl my-10 mx-auto font-mono shadow-2xl w-5/6 flex flex-col items-center justify-center ">
+        <div class="flex justify-center items-center  ">
+            <img class="object-cover h-96 my-12 " alt="hero" src="../assets/home/404.png" />
+        </div>
+        <div class="pb-12 text-center">
+            <p class=" text-3xl font-bold mb-5">Error 404 – Page Not Found</p>
+                
+              <p class="text-center">The page you requested could not be found. Perhaps searching will help.</p>  
+        </div>
+    </div>
+
+
+
+
 </template>
