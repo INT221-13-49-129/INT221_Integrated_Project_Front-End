a9e94108f2aec6020b79d4bc8b05db80177a0595
Kamin124712T <kamin.124712@mail.kmutt.ac.th>
2021-05-04 05:38:32 +0700

    Update PageNumber.vue

--- src/components/BasePageNumber.vue
+++ src/components/BasePageNumber.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="flex flex-row mx-auto mt-6 text-center justify-center text-base text-gray-100 font-thin antialiased">
-        <div @click="changPage(1)" class="mx-2  w-7 h-7 bg-black bg-opacity-80 rounded-md shadow-inner hover:bg-gray-300 hover:text-gray-900 cursor-pointer">&lt;&lt;</div>
-        <div @click="changPage(page.number)" class="mx-2  w-7 h-7 bg-black bg-opacity-80 rounded-md shadow-inner hover:bg-gray-300 hover:text-gray-900 cursor-pointer">&lt;</div>
-        <div v-for="n in page.totalPages" :key="n" @click="changPage(n)" :class="{'bg-gray-300 text-gray-900 ':this.page.number+1==n}" class="mx-2  w-7 h-7 bg-black bg-opacity-80 rounded-md shadow-inner hover:bg-gray-300 hover:text-gray-900 cursor-pointer">{{n}}</div>
-        <div @click="changPage(page.number+2)" class="mx-2  w-7 h-7 bg-black bg-opacity-80 rounded-md shadow-inner hover:bg-gray-300 hover:text-gray-900 cursor-pointer">&gt;</div>
-        <div @click="changPage(page.totalPages)" class="mx-2  w-7 h-7 bg-black bg-opacity-80 rounded-md shadow-inner hover:bg-gray-300 hover:text-gray-900 cursor-pointer">&gt;&gt;</div>
+        <div @click="changPage(1)" class="mx-1 md:mx-2    w-7 h-7 bg-black bg-opacity-80 rounded-md shadow-inner hover:bg-gray-300 hover:text-gray-900 cursor-pointer">&lt;&lt;</div>
+        <div @click="changPage(page.number)" class="mx-1 md:mx-2    w-7 h-7 bg-black bg-opacity-80 rounded-md shadow-inner hover:bg-gray-300 hover:text-gray-900 cursor-pointer">&lt;</div>
+        <div v-for="n in page.totalPages" :key="n" 
+        v-show="(n>(page.number+1) && n-(page.number+1) < 2) || (n< (page.number+1) && (page.number+1)-n < 2) || (n==page.number+1) || (n==1) || (n==page.totalPages) || (n == page.number+3 ) || (n == page.number-1) "
+        @click="changPage(n)" :class="{'bg-gray-300 text-gray-900 ':page.number+1==n}" class="mx-1 md:mx-2  w-7 h-7 bg-black bg-opacity-80 rounded-md shadow-inner hover:bg-gray-300 hover:text-gray-900 cursor-pointer">{{ ((n == parseInt(page.number)+3 || n == parseInt(page.number)-1) && (n != 1 && n != page.totalPages))  ?'...': n }}</div>
+        <div @click="changPage(page.number+2)" class="mx-1 md:mx-2    w-7 h-7 bg-black bg-opacity-80 rounded-md shadow-inner hover:bg-gray-300 hover:text-gray-900 cursor-pointer">&gt;</div>
+        <div @click="changPage(page.totalPages)" class="mx-1 md:mx-2   w-7 h-7 bg-black bg-opacity-80 rounded-md shadow-inner hover:bg-gray-300 hover:text-gray-900 cursor-pointer">&gt;&gt;</div>
     </div>
 </template>
 <script>
