becff1239ad874a6a32771d602aedc9d46fdc1f1
Kamin124712T <kamin.124712@mail.kmutt.ac.th>
2021-05-04 04:09:30 +0700

    Update App.vue menu

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,20 @@
   <div id
     class="flex flex-col font-serif antialiased bg-gradient-to-br from-dark to-red-600 min-h-screen xl:w-full md:w-336 w-screen ">
     <div id="nav" class="w-full h-12  shadow-2xl bg-black ">
-      <div class="md:mx-auto flex flex-row item-center  md:w-320">
-        <div class="md:w-4/12">
+      <div class="md:mx-auto mx-4 flex flex-row item-center md:w-320">
+        <div class="md:w-4/12 w-1/12">
           <router-link to="/">
             <img :src="logo" class="my-1.5 ml-1 h-9" />
           </router-link>
         </div>
-        <ul class="text-white md:flex-grow text-center">
+        <ul class="text-white flex-grow text-center">
           <li class="px-10 py-1.5 font-light  text-3xl">
             <router-link to="/">
               <span><b>CARS</b></span>
             </router-link>
           </li>
         </ul>
-        <ul class="my-auto flex flex-row justify-end h-full text-white font-thin text-lm md:w-4/12">
+        <ul class="hidden md:flex md:flex-row my-auto justify-end h-full text-white font-thin text-lm md:w-4/12">
           <li class="px-4">
             <router-link to="/">
               <span><b>Home</b></span>
@@ -37,6 +37,40 @@
             </router-link>
           </li>
         </ul>
+        <div class="my-auto md:hidden">
+          <div v-if="menu" @click="menu=false" class="material-icons text-right  w-1/12 mr-2 text-white text-3xl cursor-pointer">
+            menu
+          </div>
+          <div v-else  @click="menu=true" class="material-icons text-right  w-1/12 mr-2 text-white text-3xl cursor-pointer">
+            close
+            </div>
+        </div>
+      </div>
+      <div  v-if="!menu" @click="menu=true" class="md:hidden w-screen h-52 absolute bg-gray-800 shadow-2xl z-10 text-white text-xl">
+        <router-link to="/">
+        <div class="w-full h-12 my-0.5 bg-gray-900 hover:bg-black hover:opacity-80 flex items-center hover:text-red-500 cursor-pointer">
+          <span class="material-icons text-4xl mx-3">
+          home
+          </span><span> Home </span></div></router-link>
+          <router-link to="/store">
+        <div class="w-full h-12 my-1 bg-gray-900 hover:bg-black hover:opacity-80  flex items-center hover:text-red-500 cursor-pointer">
+          <span class="material-icons text-4xl mx-3">
+            directions_car_filled
+            </span><span> Store </span> 
+        </div></router-link>
+        <router-link to="/info/add">
+        <div class="w-full h-12 my-1 bg-gray-900 hover:bg-black hover:opacity-80  flex items-center hover:text-red-500 cursor-pointer">
+          <span class="material-icons text-4xl mx-3">
+            add_box
+            </span><span>Add-Car </span>           
+        </div></router-link>
+        <router-link to="/team">
+        <div class="w-full h-12 my-1 bg-gray-900 hover:bg-black hover:opacity-80  flex items-center hover:text-red-500 cursor-pointer">
+          <span class="material-icons text-4xl mx-3">
+            groups
+            </span><span> Team </span>
+        </div>
+      </router-link>
       </div>
     </div>
     <div class="w-full md:w-320 px-5 md:px-0 md:mx-auto bgimge flex-grow">
@@ -74,6 +108,7 @@
 export default {
   data() {
     return {
+      menu:!false,
       logo:require("./assets/logoCar.png"),
     };
   }
